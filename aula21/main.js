0a8b7713b2d9dbdce2067604c8830f616ba2f53c
CaioVRS21 <veras.caio12@gmail.com>
2022-06-20 19:58:27 -0400

    Exercício sobre prompt e alert realizado

---
+++ aula21/main.js
@@ -0,0 +1,7 @@
+const num1 = prompt('Digite o primeiro valor');
+const num2 = prompt('Digite o segundo valor');
+
+const num1Convertido = Number(num1);
+const num2Convertido = Number(num2);
+let res = num1Convertido + num2Convertido;
+alert(`O resultado da soma dos dois valores é de ${res}`)
