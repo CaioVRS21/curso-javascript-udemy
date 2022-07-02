f58a717974da0a2c425b4e778439af0946ec122e
CaioVRS21 <veras.caio12@gmail.com>
2022-07-02 17:25:09 -0400

    aulas 26 e 27 finalizadas

---
+++ aula26/aula26.js
@@ -0,0 +1,10 @@
+num1 = 0.7;
+num2 = 0.1;
+
+//console.log(num1.toString() + num2)
+//num1 = num1.toString()
+//console.log(num1.toString(2)) convertido para número binário
+//console.log(num1.toFixed(2)) faz com que um número float apareca com x casas decimasi visiveis passadas como parametro
+//console.log(Number.isInteger(num1))
+//console.log(Number.isNaN(num2))
+//console.log(num1 + num2) a soma de 0.7 e 0.1 resulta em 0.7999999999999 dada uma certa imprecisão do js
