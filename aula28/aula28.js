20c156cd1f33606c82db155e2ec394092d12e6f6
CaioVRS21 <veras.caio12@gmail.com>
2022-07-03 17:29:01 -0400

    Aulas 27 e 28 completas

---
+++ aula28/aula28.js
@@ -0,0 +1,15 @@
+// estudos de array
+const alunos = ['Luiz', 'Maria', 'João']
+ 
+console.log(alunos)
+//console.log(alunos.slice(0, 2)) fatia o array dentro dos parâmetros selecionados (X é o ínicio, y é o fim)
+
+//delete alunos[1] remove o conteudo do array ou index deste array, o index ficará vazio
+//alunos.shift() remove o primeiro elemento do array
+//alunos.pop() deleta o último valor do array
+//alunos.unshift('Laura') adiciona no começo do array
+//alunos.unshift('Fabio')
+//alunos.push('Marcos') adiciona um elmento no final do array
+//alunos.length
+//console.log(alunos.length) retorna o tamanho do array
+//alunos[alunos.length] = 'Lucas' adiciona um elemento no final do array
