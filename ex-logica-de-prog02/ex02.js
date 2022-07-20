544a163308161d72a817ca1db8ddd2386740414c
CaioVRS21 <veras.caio12@gmail.com>
2022-07-20 10:11:41 -0400

    Exercício 2 finalizado

---
+++ ex-logica-de-prog02/ex02.js
@@ -0,0 +1,9 @@
+//Criar uma função que retorna true caso a imagem esteja em paisagem e false caso esteja em modo retrato
+
+// function ePaisagem (largura, altura){
+//     return (largura > altura)
+// }
+
+const ePaisagem = (largura, altura) => (largura >= altura) 
+
+console.log(ePaisagem(2000, 2000))
