8744a8f67bbe4bdcaf681319b9a5c971e816a693
CaioVRS21 <veras.caio12@gmail.com>
2022-07-20 10:01:38 -0400

    Exercício 1 de lógica de programação finalizado

---
+++ ex-logica-de-prog01/ex01.js
@@ -0,0 +1,10 @@
+// //função capaz de ler dois números e retornar o maior entre os dois
+// function max (x, y){
+//     return x > y ? x : y;
+// }
+// console.log(max(50, 297))
+
+//outra solução:
+const max2 = (x, y) => x > y ? x : y;
+
+console.log(max2(900,3000))
