d092e6119520c2f18e22b5bce0bd54ba6327c923
CaioVRS21 <veras.caio12@gmail.com>
2022-07-27 20:05:26 -0400

    Aula 77 finalizada

---
+++ aula77.js
@@ -0,0 +1,19 @@
+// Funções construtoras
+//Tanto as construtoras qnto as factory criam objeto mas as construtoras são mais automatizadas em certos aspectos
+//Construtoras precisam começar com letra maiúscula e um parâmetro new
+//New automaticamente um onjeto vazio e faz o this. apontar para esse objeto vazio e retorna esse objeto, tirando a necessidade de usar return
+function Pessoa (nome, sobrenome){
+    //Essas variaveis são atributos e/ou metodos privados, inalcansaveis por notação de ponto e não podem ser acessados fora do corpo desta função
+    const ID = 12345;
+    const metodo = e => {
+        console.log('Oi')
+    };
+    //Atrinutos ou metodos públicos
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+}
+
+const p1 = new Pessoa('Caio', 'Veras');
+console.log(p1) //Retorna um objeto com as chaves nome e sobrenome
+const p2 = new Pessoa('Maria', 'Ester');
+console.log(p2.metodo);
