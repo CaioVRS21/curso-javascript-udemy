b433924748c92d3e861c95033c6579fb7b83c0ab
CaioVRS21 <veras.caio12@gmail.com>
2022-12-23 21:57:23 -0400

    Atualização

---
+++ aula118/src/index.js
@@ -0,0 +1,8 @@
+import {nome, sobrenome, idade, soma} from "./modulo1";
+
+const juntaNome = (nome, sobrenome) => {
+    return nome + ' ' + sobrenome
+}
+console.log(soma(2, 2))
+console.log(juntaNome(nome, sobrenome))
+console.log(idade)
---
+++ aula118/src/modulo1.js
@@ -0,0 +1,9 @@
+const nome = 'Caio';
+const sobrenome = 'Veras';
+const idade = 21;
+
+const soma = (x, y) => {
+    return x + y;
+}
+
+export {nome, sobrenome, idade, soma}
